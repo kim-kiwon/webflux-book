d7f1424a10ad7ae13ccd5bab3f141c697fe487b5
kiwon-k <kiwon.k@navercorp.com>
2022-12-26 15:56:43 +0900

    feat: cartItem 리스트를 갖고있는 도메인인 Cart 생성

---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/domain/Cart.java
@@ -0,0 +1,24 @@
+package com.greglturnquist.hackingspringboot.reactive.domain;
+
+import com.greglturnquist.hackingspringboot.reactive.model.CartItem;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+
+public class Cart {
+    private @Id String id;
+    private List<CartItem> cartItems;
+
+    private Cart() {}
+
+    // id만 받는 생성자
+    public Cart(String id) {
+        this(id, new ArrayList<>());
+    }
+
+    // id와 카트 아이템 목록을 받는 생성자
+    public Cart(String id, List<CartItem> cartItems) {
+        this.id = id;
+        this.cartItems = cartItems;
+    }
+}
